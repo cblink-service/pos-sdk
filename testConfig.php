0ecc8d62046038ef73065d55da75b77e7d2cc581
jinjun <757258777@qq.com>
2021-03-24 16:52:07 +0800

    [fix]:composer.json update php

---
+++ testConfig.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'JinDieXingKong' => [
+        'user_name' => '1',
+        'password' => '1',
+        'debug' => true,
+    ],
+    'JinDieSS' => [
+        'username' => '1',
+        'password' => '1',
+        'lcid' => 2052,
+        'debug' => true,
+    ],
+    'JinDieYDT' => [
+        'accessKey' => '1',
+        'secret ' => '1',
+        'debug' => true,
+    ],
+];
